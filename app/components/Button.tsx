6b0b13b14b21070ecb7fedef42189604035a1283
John Britzke <ziggynose@gmail.com>
2025-08-04 15:13:47 -0700

    import react-icons and classnames

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,9 +1,26 @@
 'use client';
 import React from 'react'
+import className from 'classnames';
 
-const Button = () => {
+interface ButtonProps {
+  children: React.ReactNode;
+  primary?: boolean;
+  secondary?: boolean;
+  disabled?: boolean;
+}
+
+const Button = ({children, primary, secondary, disabled}: ButtonProps) => {
+  const styling = className({
+    'px-4 py-2 rounded': true,
+    'bg-blue-500 text-white': primary,
+    'bg-gray-500 text-white': secondary,
+    'opacity-50 cursor-not-allowed': disabled,
+  });
+
   return (
-    <div>Button</div>
+    <button className={styling} disabled={disabled}>
+      {children}
+    </button>
   );
 }
 
